4053703a640b0f96f04e13fb71b480a4c4505ba8
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-03-26 17:34:07 -0300

    Add files via upload

---
+++ Projetil.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Projétil
+{
+
+    public class Program 
+    {
+        public static void Main()
+        {
+			const double G = 9.80665;
+			      double AnguloGraus;
+			      double AnguloRadianos;
+			      double Velocidade;
+			      double Alcance;
+			      double Altura;
+			      
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Projétil");
+			Console.WriteLine();
+			Console.ResetColor();
+			
+			Console.Write("Insira a velocidade (m/s): ");
+			Velocidade = Convert.ToDouble(Console.ReadLine());
+			
+			Console.Write("Insira o ângulo (graus): ");
+			AnguloGraus = Convert.ToDouble(Console.ReadLine());
+			
+			AnguloRadianos = AnguloGraus * (Math.PI/180);
+			
+			Alcance = (Math.Pow(Velocidade, 2) * Math.Sin(2 * AnguloRadianos)) / G;
+			Console.WriteLine($"Alcance: {Alcance:N2} m");
+			
+			Altura = Math.Pow((Velocidade * Math.Sin(AnguloRadianos)), 2) / (2 * G);
+			Console.WriteLine($"Altura: {Altura:N2} m");
+        }
+    }
+}
